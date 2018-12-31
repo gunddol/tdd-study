0d55b83b9c62f22746d3d98e37f74ab593509ab6
Yong Jae Kim <youthful2016@gmail.com>
2019-01-01 03:18:59 +0900

    #4 ch9 done

---
+++ ch9/yojkim/ch9/money_test.go
@@ -0,0 +1,14 @@
+package ch9
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCurrency(t *testing.T) {
+	assert.Equal(t, "USD", NewDollar(5).Currency())
+	assert.Equal(t, "CHF", NewFranc(5).Currency())
+
+	assert.NotEqual(t, "CHF", NewDollar(5).Currency())
+	assert.NotEqual(t, "USD", NewFranc(5).Currency())
+}
